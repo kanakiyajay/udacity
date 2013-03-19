1f28f2ba32c5c316986867d4a11abc32a90630ec
Jay Kanakiya <kanakiyajay@yahoo.com>
2013-03-19 16:55:32 +0530

    Level 1

--- main.js
+++ main.js
@@ -3,13 +3,21 @@ var stage = new Kinetic.Stage({
     width:1366,
     height:667
 });
+
+var door = new Kinetic.Circle({
+  x : 900,
+  y:  600,
+  radius : 20,
+  fill : "blue"
+});
 var layer = new Kinetic.Layer();
+var layer2 = new Kinetic.Layer();
 
 var circle = new Kinetic.Circle({
 
     x : 20,
     y: 20,
-    radius : 10,
+    radius : 9,
     fill:"black",
 });
 
@@ -18,13 +26,13 @@ var rect = new Kinetic.Rect({
   y: 300,
   width: 100,
   height: 50,
-  fill: 'green',
+  fill: 'brown',
   name:"rect",
   id : "rect1"
 });
 
 var rect2 = new Kinetic.Rect({
-  x: 700,
+  x: 600,
   y: 300,
   width: 100,
   height: 50,
@@ -33,11 +41,48 @@ var rect2 = new Kinetic.Rect({
   id : "rect2"
 });
 
-//rect.rotateDeg(30);
+var rect3 = new Kinetic.Rect({
+  x: 400,
+  y: 100,
+  width: 100,
+  height: 50,
+  draggable : true,
+  fill: 'yellow',
+  name:"rect",
+  id : "rect3",
+  dragBoundFunc: function(pos) {
+          return {
+            x: pos.x,
+            y: this.getAbsolutePosition().y
+          }
+        }
+});
 
-layer.add(circle);
+rect3.on('mouseover', function() {
+    document.body.style.cursor = 'move';
+});
+rect3.on('mouseout', function() {
+    document.body.style.cursor = 'default';
+});
+
+var rect4 = new Kinetic.Rect({
+  x: 400,
+  y: 500,
+  width: 100,
+  height: 50,
+  fill: 'green',
+  name:"rect",
+  id : "rect4"
+});
+//rect.rotateDeg(30);
+//Layer2 Contains the ball
+layer2.add(circle);
 layer.add(rect);
 layer.add(rect2);
+layer.add(rect3);
+layer.add(rect4);
+layer.add(door);
+stage.add(layer2);
 stage.add(layer);
 
 var allRects = layer.get(".rect");
@@ -53,7 +98,8 @@ for (var i = allRects.length - 1; i >= 0; i--) {
 };
 
 var centreX = 0 ,
-       timecount = 0,
+      timecount = 0,
+       timeclash = 0,
        endpoints = [
                                  {x:0,y:0,name:"up"},
                                  {x:0,y:0,name:"right"},
@@ -75,66 +121,74 @@ var anim = new Kinetic.Animation(function  (frame) {
    centreX = circle.getX();
    centreX>1360 ? directionX = "left" : null ;
    centreX< 10 ? directionX = "right" : null ;
-   directionX === "right" ?  circle.setX(centreX+5) : null ;
-   directionX === "left" ?  circle.setX(centreX-5) : null ;
+   directionX === "right" ?  circle.setX(centreX+4) : null ;
+   directionX === "left" ?  circle.setX(centreX-4) : null ;
 
    centreY = circle.getY();
    centreY>660 ? directionY = "up" : null ;
    centreY< 10 ? directionY = "down" : null ;
-   directionY === "down" ?  circle.setY(centreY+5) : null ;
-   directionY === "up" ?  circle.setY(centreY-5) : null ;
+   directionY === "down" ?  circle.setY(centreY+4) : null ;
+   directionY === "up" ?  circle.setY(centreY-4) : null ;
 
+   //For General TimeDIfference
+   if (frame.timeDiff>18) {
+   //For Clashing Time Difference
+     if (door.intersects(centreX,centreY)) {
+        anim.stop();
+        alert("You Have Won the Game");
+     };
+   if (frame.time>timeclash+200) {
    //Check Below condition for all the rectangles
-   //if (frame.time>timecount+20) {
      for (var i = 0; i < allRects.length; i++) {
         //Check For the Four Edges
            if(allRects[i].intersects(centreX,centreY))
            {
               /*There's is a clash between the brick and ball*/
-              anim.stop();
               console.log("Has Intersected the brick");
-              //timecount = frame.time;
-              //console.log(timecount);
+              timeclash = frame.time;
+              //console.log(timeclash);
               calculateEdges(function(){
                   for (var j = 0; j < endpoints.length; j++){
-
+                    //Only one point wont be intersecting.
                     if (!allRects[i].intersects(endpoints[j])) {
-                      console.log(allRects[i].intersects(endpoints[j]));
+                      console.log(endpoints[j].name);
                       changePath(endpoints[j].name);
+                      //anim.stop();
                     };
                   };
               });
            }
      };
-   //};
-},layer);
+   };
+   };
+},layer2);
 
 var changePath = function  (name) {
-  console.log(name);
+
   name==="left"||name==="right"? directionX = name : directionY = name ;
-  console.log(directionX);
-  console.log(directionY);
+
 }
 
 var calculateEdges = function  (done) {
   for (var i = 0; i < endpoints.length; i++) {
-    if(endpoints[i].name==="up")
-    {
-      endpoints[i].x = centreX;
-      endpoints[i].y = centreY-10;
-    };
-    if (endpoints[i].name==="down") {
-      endpoints[i].x = centreX;
-      endpoints[i].y = centreY+10;
-    };
-    if (endpoints[i].name==="right") {
-      endpoints[i].x = centreX+10;
-      endpoints[i].y = centreY;
-    };
-    if (endpoints[i].name==="left") {
-      endpoints[i].x = centreX-10;
-      endpoints[i].y = centreY;
-    };
+    switch(endpoints[i].name)
+      {
+      case "up":
+        endpoints[i].x = centreX;
+        endpoints[i].y = centreY-10;
+        break;
+      case "down":
+        endpoints[i].x = centreX;
+        endpoints[i].y = centreY+10;
+        break;
+      case "right" :
+        endpoints[i].x = centreX+10;
+        endpoints[i].y = centreY;
+        break;
+      default:
+        endpoints[i].x = centreX-10;
+        endpoints[i].y = centreY;
+      }
   };
   done();
 }
